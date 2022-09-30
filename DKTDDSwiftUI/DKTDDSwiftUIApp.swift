fcb4cd46aac1ed845aeced7aa815919887676369
Ramon Geronimo <ramon.geronimo@siriusxm.com>
2022-09-30 11:53:03 -0400

    Initial Commit

---
+++ DKTDDSwiftUI/DKTDDSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DKTDDSwiftUIApp.swift
+//  DKTDDSwiftUI
+//
+//  Created by Ramon Geronimo on 9/30/22.
+//
+
+import SwiftUI
+
+@main
+struct DKTDDSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
