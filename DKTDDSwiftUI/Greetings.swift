e96f3eda7bad84c76dbdc9e1d8cfcaf6a221d7cc
Ramon Geronimo <ramon.geronimo@siriusxm.com>
2022-10-01 16:42:53 -0400

    add helper methods into an extention

--- DKTDDSwiftUI/Greetings.swift
+++ DKTDDSwiftUI/Greetings.swift
@@ -8,13 +8,47 @@
 import Foundation
 
 class Greetings {
-  
+    
+    func greet(name: String? = nil, names: [String]? = nil) -> String {
+        var result = ""
+        var nameWithComas = [String]()
+        var name = name
+        var lastName = ""
+        
+        if names?.count == 1 {
+            name = names?[0]
+        }
+        
+        checkeNameWithComasOrDoubleQuotes(name, &nameWithComas)
+        
+        if nameWithComas.count >= 2 {
+            lastName = nameWithComas.last ?? ""
+            handlesArraysOfNames(nameWithComas, lastName, &result, &nameWithComas)
+            return "Hello, \(result)"
+        }
+        
+        if let name = name, name != "" && names == nil {
+            return name == name.uppercased() ? "HELLO, \(name)" : "Hello, \(name)"
+        }
+
+        if let names = names, names.count >= 2 {
+            lastName = names.last ?? ""
+            handlesArraysOfNames(names, lastName, &result, &nameWithComas)
+            return "Hello, \(result)"
+        }
+        
+        
+        return "Hello, \(names?[0] ?? "my friend")"
+    }
+}
+
+extension Greetings {
     func handleNamesCountEqualsTwo(_ names: inout [String], _ result: inout String) {
         if names[0].contains(",") {
-            var newNames = names[0].components(separatedBy: ", ")
+            let newNames = names[0].components(separatedBy: ", ")
             result += "\(newNames[0]), \(newNames[1]), and \(names[1])"
         } else if names[1].contains(","){
-            var newNames = names[1].components(separatedBy: ", ")
+            let newNames = names[1].components(separatedBy: ", ")
             result += "\(names[0]), \(newNames[0]), and \(newNames[1])"
         } else {
             result += "\(names[0]), and \(names[1])"
@@ -49,44 +83,12 @@ class Greetings {
     
     func checkeNameWithComasOrDoubleQuotes(_ name: String?, _ nameWithComas: inout [String]) {
         if  let name = name, name.contains("\"") && name.contains(",") {
-            var newNames = name.replacingOccurrences(of: "\"", with: "")
+            let newNames = name.replacingOccurrences(of: "\"", with: "")
             nameWithComas = newNames.components(separatedBy: ", ")
         }
         
         if let name = name,  name.contains(",") && !name.contains("\"") {
             nameWithComas = name.components(separatedBy: ", ")
         }
-    }
-    
-    func greet(name: String? = nil, names: [String]? = nil) -> String {
-        var result = ""
-        var nameWithComas = [String]()
-        var name = name
-        var lastName = ""
-        
-        if names?.count == 1 {
-            name = names?[0]
-        }
-        
-        checkeNameWithComasOrDoubleQuotes(name, &nameWithComas)
-        
-        if nameWithComas.count >= 2 {
-            lastName = nameWithComas.last ?? ""
-            handlesArraysOfNames(nameWithComas, lastName, &result, &nameWithComas)
-            return "Hello, \(result)"
-        }
-        
-        if let name = name, name != "" && names == nil {
-            return name == name.uppercased() ? "HELLO, \(name)" : "Hello, \(name)"
-        }
-
-        if let names = names, names.count >= 2 {
-            lastName = names.last ?? ""
-            handlesArraysOfNames(names, lastName, &result, &nameWithComas)
-            return "Hello, \(result)"
-        }
-        
-        
-        return "Hello, \(names?[0] ?? "my friend")"
     }
 }
